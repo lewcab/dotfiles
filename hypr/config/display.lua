4a17866251df1d79bb2f1c605ad6a89bf2ed49b2
lewcab <lewcabal@gmail.com>
2026-05-15 23:27:46 -0700

    updated display and theme

--- hypr/config/display.lua
+++ hypr/config/display.lua
@@ -1,7 +1,7 @@
 -- https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
-	output = "",
-	mode = "preferred",
-	position = "auto",
+	output = "eDP-1",
+	mode = "1920x1080@60",
+	position = "0x0",
 	scale = "1",
 })
